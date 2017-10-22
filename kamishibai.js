e603427d83beac81896695bc2a11a5565f2551c7
t-noji <t.nojinoji@gmai.com>
2017-10-22 22:53:53 +0900

    first touch

---
+++ kamishibai.js
@@ -0,0 +1,144 @@
+'use strict'
+const script = (parent, obj)=>{
+
+const
+  element = $mk('div',{style: {position: 'absolute',
+                  width:  ((obj && obj.width)  || 640) +'px',
+                  height: ((obj && obj.height) || 480) +'px'}}),
+  background = $mk('img',{style:{position: 'absolute', height: '100%', width: '100%'}}),
+  layer_ele = $mk('div',{style:{position: 'absolute',overflow: 'hidden', height: '100%', width: '100%'}}),
+  filter_ele = $mk('div',{style:{position: 'absolute', height: '100%', width: '100%'}}),
+  front_ele = $mk('div',{style:{position: 'absolute', height: '100%', width: '100%'}}),
+  text_ele = $mk('div',{style:{position: 'absolute', height: '100%', width: '100%'}}),
+  fukidasi = {className: 'fukidashi',style:
+        {position: 'absolute', bottom: '0px', left: '10%', width: '80%', height: '5em',
+         color: 'white', textShadow: '0 1px 3px black', fontSize: '140%'}},
+  hito = {}
+
+element.appendChild(background)
+element.appendChild(layer_ele)
+element.appendChild(filter_ele)
+element.appendChild(text_ele)
+element.appendChild(front_ele)
+parent.appendChild(element)
+
+addOn(n_list,{
+  'this-box': element,
+  hito: hito,
+  front: front_ele
+})
+addIn(n_list, {
+  setParent: parent=> parent.appendChild(element),
+  'resize-box': (w,h)=>
+    addIn(element.style, {width: w +'px', height: h +'px'}),
+  'resize-font': a=> element.style.fontSize = a,
+  image: src=> addIn(new Image(), {src: src}),
+  bg: (img,animation)=>{
+    background.src = img.src
+    background.style.animation = 'none'
+    background.style.animation = animation || 'fadein 0.75s'
+  },
+  make: (name, ...ks)=>
+    hito[name] = duo(ks).reduce((pre,k)=>
+      addOn(pre, {[k[0]]: addIn(k[1],
+        {className: name, style: {position: 'absolute', width: 'auto'}})}), {}),
+  filter: fil=>
+    fil
+      ? typeof fil === 'string'
+        ? filter_ele.style.backgroundColor = fil || "rgba(0,0,0,0)"
+        : filter_ele.style.backgroundImage = `url('${fil.src}')`
+      : addIn(filter_ele,{style:{backgroundColor: 'transparent',
+                                 backgroundImage: 'none'}}),
+  clear: (...arg)=>
+    arg.length
+      ? arg.forEach(a=> $remove(layer_ele.getElementsByClassName(a)[0]))
+      :  [... layer_ele.children, $getClass(element, 'select')].forEach($remove),
+  show: (name, ...ps)=>{
+    clear(name)
+    layer_ele.appendChild(addIn(...ps.map(p=> hito[name][p])))
+  },
+  talk: (name,str)=>{
+    const fd =  mix(fukidasi, {textContent: `${name}\n「${str}」`})
+    $getClass(layer_ele, name)
+    addIn(text_ele, fd)
+  },
+  text: str=> addIn(text_ele, mix(fukidasi, {textContent: str})),
+  右: {style: {left: '60%'}},
+  左: {style: {right: '60%'}},
+  中: {style: {top: 0, left: 0, right: 0, margin: 'auto'}},
+  上下: {style: {animation: 'vertical 0.5s linear infinite alternate'}},
+  停止: {style: {animation: 'none'}}
+})
+
+
+const splitArray = (l,key,i)=>
+  (i= l.indexOf(key)) === -1
+    ? [l]
+    : concat([l.slice(0, i)], splitArray(l.slice(i +1), key))
+
+addIn(n_list, {'split-array': splitArray})
+
+// マクロの順番に注意 / 展開タイミングに影響します /
+exec(`
+  (defmacro chapter (name & body)
+   \`(defun ,name ()
+       (script-eval ,@body)))
+
+  (defmacro script-eval (& body)
+   \`(let ((l (quote ,@(split-array body "wt")))
+           (i 0))
+       (set this-box "onclick"
+         (lambda (e) (list-progn (nth l (incf i)))))
+       (defun onkeypress (e)
+         (if (eq (get e "keyCode") 32)
+           (list-progn (nth l (incf i)))))
+       (list-progn (first l))))
+
+  (defmacro shows (& body)
+   \`(show ,@(map body (lambda (b) (list 'str b)))))
+
+  (defun wt () (undefined))
+
+  (defun switch (& body)
+    (set this-box "onclick" through)
+    (def onkeypress through)
+    (duo body (lambda (o f)
+                (append-child
+                  front
+                  (set ($mk "div" {className: "switch"} o) "onclick" f))
+                (append-child front ($mk "br")))))
+
+  (defun select (& body)
+    (set this-box "onclick" through)
+    (def onkeypress through)
+    (let ((ele (append-child front ($mk "div" {className: "select"}))))
+      (duo body (lambda (o f)
+                  (append-child
+                    ele
+                    (set ($mk "div" {className: "switch"} o)
+                      "onclick" (lambda (e)
+                                  ($rm ele)
+                                  (f e))))
+                  (append-child ele ($mk "br"))))))
+
+`)
+
+reader_macro.push(str=>
+  str.replace(/^\s*([^\s(]+)「(.*)」/gm, '(talk (str $1) (str $2)) wt '))
+reader_macro.push(str=>
+  str.replace(/^\s*[＊　]([^\s]*)/gm, '(text (str $1)) wt '))
+
+// 日本語別名登録 //
+addIn(n_list, {
+  人物: n_list.make,
+  背景: n_list.bg,
+  消去: n_list.crear,
+  スイッチ: n_list['switch'],
+  分岐: n_list['select']
+})
+addIn(macro, {
+  表示: macro.shows,
+  章: macro.chapter,
+})
+
+}
---
+++ lisp.js
@@ -0,0 +1,275 @@
+'use strict'
+const
+  addIn = (obj, ...arg)=>
+    (arg.forEach(a=>
+      a && Object.keys(a).forEach(k=>
+        a[k] instanceof Object && typeof a[k] !== 'function'
+          ? obj[k]
+            ? addIn(obj[k], a[k])
+            : addIn(obj[k] = {}, a[k])
+          : obj[k] = a[k])),
+     obj),
+  mix = (...args) => addIn({}, ...args),
+  addOn = (obj, ...arg)=>
+    (arg.forEach(a=>
+      a && Object.keys(a).forEach(k=> obj[k] = a[k])),
+     obj),
+  even = n=> !(n%2),
+  odd = n=> n%2,
+  duo = (l, f=(x,y)=>[x,y])=>
+    l.reduce((pre,a,i)=> pre.concat(i%2 ? [] : [f(a, l[i+1])]) ,[])
+
+const {
+  n_list, // lisp内で利用可能な関数リスト
+  macro,  // lisp内で利用可能なマクロリスト
+  reader_macro, // リーダマクロリスト
+  exec // (String body)=>result / lisp実行用関数
+} = (()=>{
+
+const n_list = addIn(window, {
+  through: a=> a,
+  '+': (a,b)=> a+b,
+  '-': (a,b)=> a-b,
+  '*': (a,b)=> a*b,
+  '/': (a,b)=> a/b,
+  '%': (a,b)=> a%b,
+  '=': (a,b)=> a===b,
+  '!=': (a,b)=> a!==b,
+  list: (...a)=> a,
+  string: String,
+  'add-in': addIn,
+  'add-on': addOn,
+  first: a=> a[1],
+  second: a=> a[2],
+  third: a=> a[3],
+  nth: (a,n)=> a[n],
+  even: even,
+  odd: odd,
+  mix: mix,
+  duo: duo,
+  apply: (f,b)=> f(...b),
+  append: (...args)=> [].concat(...args),
+  log: a=> (console.log(a), a)
+})
+
+const macro ={
+  defmacro: (name, namelist, body)=>{
+    macro[name] = (...args)=>
+      macroexpand(exe(argsReplace(body, namelist, args)))
+    return ['undefined']
+  },
+  backquote:
+   ((lflat = (l, key, f=x=>x)=>
+      l.indexOf(key) === -1
+        ? ['list', ...l.map(f)]
+        : ['append', ...l.map((i,index)=>
+            i === key
+              ? false
+              : l[index -1] === key
+                ? i
+                : ['list', f(i)])].filter(Boolean),
+     bq = s=>
+       !Array.isArray(s)
+         ? (typeof s === 'string' && s !== '@') || s.closure === s.closure
+           ? ['quote', s]
+           : s
+         : s.length === 0
+           ? ['quote', []]
+           : s[0] === 'unquote'
+             ? s.length === 2
+               ? s[1]
+               : s.slice(1)
+             : lflat(s, '@', bq))=>
+    (...args)=>
+      args.length === 1
+        ? ['quote', args[0]]
+        : lflat(args, '@', bq)
+  )(),
+}
+
+const
+  closure = {}, // closure_key
+  macroexpand = b=> // ((a 1)) 
+    !Array.isArray(b) || b.length === 0
+      ? b.closure === closure
+        ? b.name
+        : b
+      : b[0] in macro
+        ? macro[b[0]](... b.slice(1).map(macroexpand))
+        : b.map(macroexpand),
+    
+  found = str=>
+    typeof str === 'function'
+      ? str
+      : str.closure === closure
+        ? str.val
+        : str.split('.').reduce((o,s)=>
+            (o[s] === undefined
+              && console.log('無いよ:', str),
+             o[s].bind)
+               ? o[s].bind(o) 
+               : o[s], n_list),
+  exe = b=>
+    !Array.isArray(b)
+      ? typeof b === 'string' || b.closure
+        ? found(b)
+        : b
+      : b[0] in special
+          ? special[b[0]](... b.slice(1))
+          : ((f = (Array.isArray(b[0])
+                   ? exe
+                   : found)(b[0]))=>
+              (typeof f !== 'function'
+                 && console.log('関数じゃないよ:', b[0], 'from', b),
+               f(... b.slice(1).map(exe))))()
+
+const argsReplace = (l, names =[], vals)=>{
+  const slice_index = (names.indexOf('&') +1) || names.length
+  return names.slice(0, slice_index).reduce((pre,name,i)=>
+    name === '&'
+      ? replace(pre, names[i+1], vals.slice(i))
+      : replace(pre, name, vals[i]),
+    l)
+}
+const replace = (l,name,val)=>{
+  const
+    closure_obj = {name: name, closure: closure,
+                   val: val.closure === closure ? val.val : val},
+    re = l=>
+      Array.isArray(l)
+        ? l.map(re)
+        : l === name || (l.closure === closure && l.name === name)
+          ? closure_obj
+          : l
+  return re(l)
+}
+// 特殊式
+const special = {
+  progn: (...body)=> body.map(exe)[body.length - 1],
+  'list-progn': body=> special.progn(...exe(body)),
+  'if': (flag, tbody, fbody=[])=> exe(exe(flag) ? tbody : fbody),
+  lambda: (names, ...body)=>{
+    const f = (...args)=>
+      special.progn(... argsReplace(body, names, args))
+    f.toString = ()=> `${names}-> `+ JSON.stringify(body)
+    return f
+  },
+  def: (...arg)=>
+    arg.forEach((a,i)=>
+      !(i%2) // hit 0 2 4 ...
+      && (a.split('.').reduce(
+        (pre,s,j,aa)=>
+          pre[s] = aa[j+1]
+                      ? pre[s] || {}
+                      : exe(arg[i+1]),
+        n_list))),
+  'let': (na, ...body)=>
+      special.lambda(na.map(a=>a[0]), ...body)
+        (...na.map(a=> exe(a[a.length -1]))),
+  setq: (cobj, body)=> exe(['quote', cobj]).val = exe(body),
+  quote: (...s)=> s.length === 1 ? s[0] : s,
+  str: String,
+  'undefined': ()=> undefined
+}
+
+// 評価
+const
+  search = (str, regex, count = -1, i)=>
+     (i = str.search(regex)) === -1
+        ? []
+        : [[count + i + 1, str[i]]].concat(search(str.slice(i + 1), regex, count + i + 1)),
+
+  esc = (str, arg_obj)=>{
+    const {start, count_start = start, start_replace = start,
+           end = '\\)', end_replace = end.replace('\\',''), outFn =s=>s, inFn =s=>s}
+             = arg_obj
+    const se = new RegExp(start +'|'+ end)
+    const ce = new RegExp(count_start +'|'+ end)
+    const escf = (str, count = 0,  i,ss,sl,el)=>
+       (i = str.search(count ? ce : se),
+        ss = (count ? count_start : start).replace(/\\/g,''),
+        sl = ss.length,
+        el = end.replace(/\\/g,'').length,
+          i === -1
+               ? str && outFn(str)
+               : str.slice(i, i+sl) === ss
+                 ? count === 0
+                   ? outFn(str.slice(0,i)) + start_replace + escf(str.slice(i+sl), 1)
+                   : inFn(str.slice(0,i+sl)) + escf(str.slice(i+sl), count +1)
+                 // end
+                 : count === 0
+                   ? outFn(str.slice(0,i+el)) + escf(str.slice(i+el), 0)
+                   : count === 1
+                     ? inFn(str.slice(0,i)) + end_replace + escf(str.slice(i+el), count -1)
+                     : inFn(str.slice(0,i+el)) + escf(str.slice(i+el), count -1))
+    return escf(str)
+  },
+  escOne = (str, key, inFn =s=>s, outFn =s=>s)=>{
+    const l = [-1, ...search(str, /"/).map(x=>x[0])]
+    return l.reduce((p,c,i)=>p+ (i % 2 ? inFn : outFn)(str.slice(c+1, l[i+1])), '')
+  },
+  readerFirstMacro = (marker,macro)=>
+    str=>
+      str.replace(RegExp(`${marker}\\(`, 'g'),`(${macro} `)
+         .replace(RegExp(`${marker}(\\S+)`, 'g'), `(${macro} $1)`)
+
+const reader_macro = [
+  readerFirstMacro("'", 'quote'),
+  readerFirstMacro('`', 'backquote'),
+  str=> str.replace(/,@/g, '@ '),
+  readerFirstMacro(',', 'unquote')
+]
+
+const exec = str=>{
+  const change = str=>
+         reader_macro.reduce((p,f)=>f(p), str)
+            .replace(/\)\s+\(/g, '),(')
+            .replace(/\(/g, '[')
+            .replace(/\)/g, ']')
+            .replace(/\s+/g, ',')
+            .replace(/[^\d\[\],][^,\[\]]*/g, '"$&"') //simbol
+            .replace(/,+]/g, ']')
+  const
+   comment_str = str.replace(/;.*$/gm, ''),
+   object_esc_str =
+     esc(comment_str, {start: '{', end: '}',
+      outFn: s=>  /"/.test(s)
+        ? escOne(s, /"/, s=>`["str","${s}"]`, change)
+        : change(s),
+      inFn: s=> s.replace(/[^\d:\s][^:\s]*(?=:)/g, '"$&"')}),
+   json = object_esc_str.replace(/^,*/, '')
+                        .replace(/,*$/, '')
+  console.log('json:', json)
+  const
+   jp = JSON.parse(`[${json}]`),
+   expanded = jp.map(macroexpand)
+  console.log('expanded macro:',expanded)
+  return special.progn(... expanded)
+}
+
+// lisp //
+exec(`
+(defmacro defun (name arglist & body)
+  \`(def ,name (lambda ,arglist ,@body)))
+
+(defmacro incf (cobj)
+  \`(setq ,cobj (+ ,cobj 1)))
+`)
+
+// 日本語命令 //
+addIn(special, {
+  もし: special["if"],
+  代入: special.def,
+  関数: special.lambda
+})
+addIn(n_list, {
+  合成: n_list.mix
+})
+
+return {
+  n_list: n_list,
+  macro: macro,
+  exec: exec,
+  reader_macro: reader_macro,
+}
+})();
